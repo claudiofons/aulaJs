1226b52bca7ed208fdafb0ddcbaa6408fdf40c70
claudiofons <claudiofons@gmail.com>
2020-06-05 03:44:07 -0300

    começando Js e DOM

---
+++ js/main.js
@@ -0,0 +1,14 @@
+function clicou() {
+  document.getElementById("agradecimento").innerHTML = "Obrigado!";
+  //alert("Obrigado por clicar!");
+}
+
+/*
+var name = "Claudio fonseca";
+alert("Bem vindo! " + name);
+
+function soma(n1, n2) {
+  return n1 + n2;
+}
+alert(soma( 3, 10));
+*/
